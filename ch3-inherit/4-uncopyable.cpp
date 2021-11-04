971dc9b932d2c1a67263c5812a7753607cb1e272
rongfan <15670381505@163.com>
2021-11-05 01:14:34 +0800

    update

--- ch3-inherit/4-uncopyable.cpp
+++ ch3-inherit/4-uncopyable.cpp
@@ -13,7 +13,50 @@ using namespace std;
  *
  * 设计一个类：
  *     类的对象没有办法进行拷贝
- *
+ *     拷贝分为两种，1）拷贝构造 2）赋值运算
+ * 功能类表示了一类类的性质
  */
 
+#define BEGINS(x) namespace x {  // namespace of x
+#define ENDS(x) } // end of namespace
 
+BEGINS(test1)
+
+
+class UNCOPYABLE {
+public:
+    // 不可以生成对象
+    // 其所有的派生类也没有拷贝构造方法
+    UNCOPYABLE(const UNCOPYABLE &) = delete;
+
+    UNCOPYABLE &operator=(const UNCOPYABLE &) = delete;
+    UNCOPYABLE &opetator=(const UNCOPYABLE &) const = delete;
+protected: 
+    // 将默认构造声明为受保护权限
+    UNCOPYABLE() = default;
+};
+
+
+class A: public UNCOPYABLE {
+    
+};
+
+
+int main() {
+    A a;
+    A b;
+    // b = a;
+    // A b(a); // no, copy constructor delete
+    return 0;
+}
+
+
+
+
+ENDS(test1)
+
+
+int main() {
+    test1::main();
+    return 0;
+}
