9eb68e3f6c7c00412a2c3fa82f86779307f06aaa
rongfan <15670381505@163.com>
2021-11-14 15:24:10 +0800

    update

---
+++ ch4-Polymorphism/3.dynamic_cast.cpp
@@ -0,0 +1,67 @@
+/*************************************************************************
+	> File Name: 3.dynamic_cast.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Sun 14 Nov 2021 02:52:28 PM CST
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+#define BEGINS(x) namespace x {
+#define ENDS(x) }
+
+BEGINS(test1)
+
+class Base {
+public:
+    virtual ~Base() {}
+};
+
+class A: public Base {
+
+};
+class B: public Base {
+
+};
+class C: public Base {
+
+};
+
+int main() {
+    srand(time(0));
+
+    Base *p;
+    switch (rand() % 4) {
+        case 0: p = new A(); break;
+        case 1: p = new B(); break;
+        case 2: p = new C(); break;
+        case 3: p = nullptr; break;
+    }
+    // 类似python中 isinstance()
+    // c++ 中dynamic_cast依据虚函数表的来判断不同对象的类型
+    A a;
+    // 如果p不是空地址
+    if (p) ((void **)(p))[0] = ((void **)(&a))[0];
+    if (dynamic_cast<A *>(p) != nullptr) {
+        cout << "p pointer A class object" << endl;
+    } else if (dynamic_cast<B *>(p) != nullptr) {
+        cout << "p pointer B class object" << endl;
+    } else if (dynamic_cast<C *>(p)) {
+        cout << "p pointer C class object" << endl;
+    } else {
+        cout << "p is nullptr" << endl;
+    }
+
+
+    return 0;
+}
+
+ENDS(test1)
+
+
+int main() {
+    test1::main();
+
+    return 0;
+}
